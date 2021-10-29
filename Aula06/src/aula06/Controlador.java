14f6c20d33668b2c9c1ce1b374592b13e860dda9
vitorosouza <vitorosouza@hotmail.com>
2021-10-29 17:10:44 -0300

    Atualização de Código

--- Aula06/src/aula06/Controlador.java
+++ Aula06/src/aula06/Controlador.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula06;
 
-/**
- *
- * @author usuario
- */
-public interface Controlador {
-    
+//1ª PARTE - CRIAR A INTERFACE
+public interface Controlador 
+{
+    //Métodos Abstratos
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
 }
+
+
+
--- Aula06/src/aula06/ControleRemoto.java
+++ Aula06/src/aula06/ControleRemoto.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula06;
-
-/**
- *
- * @author usuario
- */
+//2ª PARTE - CRIAR 
 public class ControleRemoto {
+    //Atributos
+    private int volume;
+    private boolean ligando;
+    private boolean tocando;
+    //Métodos Especiais
+    
+    
     
 }
