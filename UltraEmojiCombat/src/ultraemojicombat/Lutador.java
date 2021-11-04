e4c5f6a38be56d4f675a9af4589f1bb4e268e11a
vitorosouza <vitorosouza@hotmail.com>
2021-11-04 01:12:42 -0300

    Progresso da programação

--- UltraEmojiCombat/src/ultraemojicombat/Lutador.java
+++ UltraEmojiCombat/src/ultraemojicombat/Lutador.java
@@ -7,60 +7,51 @@ public class Lutador {
     private float altura;
     private float peso;
     private String categoria;
-    private int vitorias;
-    private int derrotas;
-    private int empates;
-    
-    //MÉTODOS CRIADOS a partir do que se dar pra "fazer" com essa classe atual.
+    private int vitorias, derrotas, empates;
+    //MÉTODOS PÚBLICOS
     public void apresentar() {
-        System.out.println("Lutador: " + getNome());
-        System.out.println("Origem: " + getNacionalidade());
-        System.out.println(getIdade() + " anos");
-        System.out.println(getAltura() + " m de altura");
-        System.out.println("Pesando: " + getPeso() + "Kg");
-        System.out.println("Ganhou: " + getVitorias());
-        System.out.println("Perdeu: " + getDerrotas());
-        System.out.println("Empatou: " + getEmpates());
+        System.out.println("-------------------------------------------");
+        System.out.println("CHEGOU A HORA! Apresentamos o lutador" + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println(this.getIdade() + " anos");
+        System.out.println(this.getAltura() + " m de altura");
+        System.out.println("Pesando: " + this.getPeso() + "Kg");
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Perdeu: " + this.getDerrotas());
+        System.out.println("Empatou: " + this.getEmpates());
     }
     //Como se fosse uma apresentação mais suscinta das informações do lutador.
     public void status() {
-        System.out.println(getNome());
-        System.out.println("é um peso " + this.categoria);
-        System.out.println(getVitorias() + "vitórias");
-        System.out.println(getDerrotas() + "derrotas");
-        System.out.println(getEmpates() + "empates");
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println(this.getVitorias() + "vitórias");
+        System.out.println(this.getDerrotas() + "derrotas");
+        System.out.println(this.getEmpates() + "empates");
     }
 
     public void ganharLuta() {
-        setVitorias(getVitorias() + 1);
+        this.setVitorias(this.getVitorias() + 1);
     }
 
     public void perderLuta() {
-        setDerrotas(getDerrotas() + 1);
+        this.setDerrotas(this.getDerrotas() + 1);
     }
 
     public void empatarLuta() {
-        setEmpates(getEmpates() + 1);
+        this.setEmpates(this.getEmpates() + 1);
     }
     
     //MÉTODOS ESPECIAIS
     //Construtor
-    public Lutador(String no,
-            String na,
-            int id,
-            float al,
-            float pe,
-            int vi,
-            int de,
-            int em) {
-        nome = no;
-        nacionalidade = na;
-        idade = id;
-        altura = al;
-        setPeso(pe);
-        vitorias = vi;
-        derrotas = de;
-        empates = em;
+    public Lutador(String no, String na, int id, float al,
+        float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
     }
     //GETTERS E SETTERS
     public String getNome() {
@@ -101,24 +92,24 @@ public class Lutador {
 
     public void setPeso(float pe) {
         this.peso = pe;
-        setCategoria(categoria);
+        this.setCategoria();
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
-        if (peso<52.2){
-            categoria = "Inválido";
-        } else if (peso <= 70.3) {
-            categoria = "Leve";
-        } else if (peso <= 83.9){
-            categoria = "Médio";
-        } else if (peso <= 120.2){
-            categoria = "Pesado";
+    public void setCategoria() {
+        if (this.peso<52.2){
+            this.categoria = "Inválido";
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9){
+            this.categoria = "Médio";
+        } else if (this.peso <= 120.2){
+            this.categoria = "Pesado";
         } else{
-            categoria = "Inválido";
+            this.categoria = "Inválido";
         }
        // this.categoria = categoria; 
     }
