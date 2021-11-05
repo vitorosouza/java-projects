a49996fa36ce6e054346fffda1b5ea950f15981e
vitorosouza <vitorosouza@hotmail.com>
2021-11-05 12:03:17 -0300

    Criação da classe Luta e continuação da programação

---
+++ UltraEmojiCombat/src/ultraemojicombat/Luta.java
@@ -0,0 +1,93 @@
+package ultraemojicombat;
+//Precisei importar para poder criar o objeto do tipo Random
+import java.util.Random;
+
+public class Luta {
+    //Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+    //Métodos Publicos
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        //Se a categoria do l1 for igual a do l2
+        if (l1.getCategoria().equals(l2.getCategoria())
+                // e l1 for diferente de l2
+                && l1 != l2)
+        {
+            //Faça
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+            //Senão
+        } else
+        {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
+
+    }
+    public void lutar(){
+        if(this.aprovada){
+            System.out.println("### DESAFIADO ###");
+            this.desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
+            
+            //Criando um objeto do tipo Random
+            Random aleatorio = new Random();
+            //Gerando 3 resultados (0 1 2) p/ definir o vencedor
+            int vencedor = aleatorio.nextInt(3);
+            System.out.println("======== RESULTADO DA LUTA ========");
+            //Estrutura "escolha"
+            switch(vencedor){
+                case 0: // Empate
+                    System.out.println("Empatou!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                    
+                case 1: // Desafiado vence
+                    System.out.println("Vitória do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                    
+                case 2: // Desafiante vence
+                    System.out.println("Vitória do " + this.desafiante.getNome());
+                    this.desafiante.ganharLuta();
+                    this.desafiado.perderLuta();
+                    break;
+            }
+                System.out.println("===================================");
+        } else {
+            System.out.println("A luta não pode acontencer!");
+        }
+    }
+    //Métodos Especiais
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+    public int getRounds() {
+        return rounds;
+    }
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+    public boolean getAprovada() {
+        return aprovada;
+    }
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+}
--- UltraEmojiCombat/src/ultraemojicombat/Lutador.java
+++ UltraEmojiCombat/src/ultraemojicombat/Lutador.java
@@ -22,24 +22,22 @@ public class Lutador {
     }
     //Como se fosse uma apresentação mais suscinta das informações do lutador.
     public void status() {
+        System.out.println("----------------------------------");
         System.out.println(this.getNome() + " é um peso " + this.getCategoria());
         System.out.println(this.getVitorias() + " vitórias");
         System.out.println(this.getDerrotas() + " derrotas");
         System.out.println(this.getEmpates() + " empate(s)");
+        System.out.println("----------------------------------");
     }
-
     public void ganharLuta() {
         this.setVitorias(this.getVitorias() + 1);
     }
-
     public void perderLuta() {
         this.setDerrotas(this.getDerrotas() + 1);
     }
-
     public void empatarLuta() {
         this.setEmpates(this.getEmpates() + 1);
     }
-    
     //MÉTODOS ESPECIAIS
     //Construtor
     public Lutador(String no, String na, int id, float al,
@@ -58,48 +56,37 @@ public class Lutador {
     public String getNome() {
         return nome;
     }
-
     public void setNome(String no) {
         this.nome = no;
     }
-
     public String getNacionalidade() {
         return nacionalidade;
     }
-
     public void setNacionalidade(String na) {
         this.nacionalidade = na;
     }
-
     public int getIdade() {
         return idade;
     }
-
     public void setIdade(int id) {
         this.idade = id;
     }
-
     public float getAltura() {
         return altura;
     }
-
     public void setAltura(float al) {
         this.altura = al;
     }
-
     public float getPeso() {
         return peso;
     }
-
     public void setPeso(float pe) {
         this.peso = pe;
         this.setCategoria();
     }
-
     public String getCategoria() {
         return categoria;
     }
-
     public void setCategoria() {
         if (this.peso<52.2){
             this.categoria = "Inválido";
@@ -114,27 +101,21 @@ public class Lutador {
         }
        // this.categoria = categoria; 
     }
-
     public int getVitorias() {
         return vitorias;
     }
-
     public void setVitorias(int vi) {
         this.vitorias = vi;
     }
-
     public int getDerrotas() {
         return derrotas;
     }
-
     public void setDerrotas(int de) {
         this.derrotas = de;
     }
-
     public int getEmpates() {
         return empates;
     }
-
     public void setEmpates(int em) {
         this.empates = em;
     }
