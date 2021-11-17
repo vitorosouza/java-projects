096322bb77e0efe320767767c107ecb52645c170
vitorosouza <vitorosouza@hotmail.com>
2021-11-17 09:27:50 -0300

    Update repos

---
+++ OlaMundoJavaFX/src/olamundojavafx/FXMLDocumentController.java
@@ -0,0 +1,27 @@
+package olamundojavafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMensagem;
+    private Button btnClick;
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Olá, mundo!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
