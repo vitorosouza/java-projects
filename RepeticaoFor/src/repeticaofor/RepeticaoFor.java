6227fdf41390287f443e3ef951fa3afe264e47b8
vitorosouza <vitorosouza@hotmail.com>
2021-12-09 17:34:55 -0300

    Adiciona repositórios

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,28 @@
+package repeticaofor;
+
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+       
+        // LAÇO FOR/PARA
+        
+        for (int cc=0; cc<=100; cc+= 10){
+            System.out.println(cc);
+        }
+        
+        
+        // LAÇO WHILE/ENQUANTO
+        /*
+        int cc=0;
+        while (cc<4){
+            System.out.println("Cambalhota");
+            cc++;
+        }
+        */
+        
+        
+        
+        
+    }
+    
+}
--- Teste/src/teste/Teste.java
+++ Teste/src/teste/Teste.java
@@ -2,20 +2,25 @@ package teste;
 
 public class Teste {
     public static void main(String[] args) {
-        char c = 'c';
-        int i = 10;
-        double d = 10;
-        long l = 1;
-        String s = "Hello";
+               
+        /*
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 0; j <= 2; j += 2) {
+                System.out.println("I = " + i + "  ~~~~~~  J = " + j);
+                
+            }
+        }
+        */
         
-        //c = c + i;
-        s += i;
-        //i += s;
-        //c += s;
-        i += l;
+        for (int c = 33; c > 13; c -= 2){
+            System.out.println(c);
+        }
+      
         
-        System.out.println(s);
-        System.out.println(i);
+        
+        
+        
+        
     }
     
 }
