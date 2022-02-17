f3190d7e842dc8e1f4c867ab3a971512143ce7d4
vitorosouza <vitorosouza@hotmail.com>
2022-02-17 14:30:23 -0300

    Adiciona programa que mostra como dividir um Array e extrair os dados

--- Primeiro-Programa-Java/src/cursojava/executavel/SplitArray.java
+++ Primeiro-Programa-Java/src/cursojava/executavel/SplitArray.java
@@ -1,5 +1,8 @@
 package cursojava.executavel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
 	public static void main(String[] args) {
@@ -14,5 +17,30 @@ public class SplitArray {
 		System.out.println("Nota 2: "+ valoresArray[3]);
 		System.out.println("Nota 3: "+ valoresArray[4]);
 		System.out.println("Nota 4: "+ valoresArray[5]);
+		
+		
+		//Convertendo um Array em uma Lista
+		
+		
+		List<String> list = Arrays.asList(valoresArray);
+		
+		for (String valorString : list) {
+			System.out.println(valorString);
+			
+			
+		}
+		
+		
+		//Convertendo uma Lista para uma Array
+		
+		String[] conversaoArray = list.toArray(new String[6]);
+		
+		for(int pos = 0; pos < conversaoArray.length; pos ++) {
+			System.out.println(conversaoArray[pos]);
+		}
+		
+		
+		
+		
 	}
 }
