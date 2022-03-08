657e4365cd159ee771b5dd50fc3cc06f67b15518
vitorosouza <vitorosouza@hotmail.com>
2022-03-08 17:22:59 -0300

    Adicionando comentários e trocando nome do arquivo

--- ArquivosJava/src/cursojava/arquivos/ApachePoi_Adicionar.java
+++ ArquivosJava/src/cursojava/arquivos/ApachePoi_Adicionar.java
@@ -11,29 +11,29 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 
-public class ApachePoi_Editar {
+public class ApachePoi_Adicionar {
 
 	public static void main(String[] args) throws Exception {
 		
-		File file = new File("C:\\workspace\\java-projects\\ArquivosJava\\src\\cursojava\\arquivos\\arquivo.xls");
-		FileInputStream entrada = new FileInputStream(file);
+		File file = new File("C:\\workspace\\java-projects\\ArquivosJava\\src\\cursojava\\arquivos\\arquivo.xls"); //Deixa o arquivo em stand-by
+		FileInputStream entrada = new FileInputStream(file); //Estancia o objeto "entrada"
 		
 		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada); //Prepara a entrada do arquivo Excel
-		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); //Pegando a planilha
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); //Pegando a planilha a partir do índice 0 (1ª página de trabalho)
 		
 		Iterator<Row> linhaIterator = planilha.iterator();//Iterator para correr nas linhas
 		
 		while(linhaIterator.hasNext()) { //Enquanto tiver linhas no arquivo
 			Row linha = linhaIterator.next(); //Dados da pessoa na linha
 			
-			int numeroCelulas = linha.getPhysicalNumberOfCells(); //Quantidade de células 
+			int numeroCelulas = linha.getPhysicalNumberOfCells(); //Pega a quantidade de células 
 			
 			Cell cell = linha.createCell(numeroCelulas); //Criar uma nova célula na linha 
-			cell.setCellValue("5.487,27");
+			cell.setCellValue("5.487,27"); //Seta o valor da nova célula
 			
 		}
 		
-		entrada.close();
+		entrada.close(); //Fecha esse input stream e libera qualquer recurso de sistema que esteja associado com ele
 		
 		FileOutputStream saida = new FileOutputStream(file);
 		hssfWorkbook.write(saida);
